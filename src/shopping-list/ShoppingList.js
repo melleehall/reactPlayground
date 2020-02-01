dfde7ec7731f6870a03bacbf3cb3e94df7325bff
Melanie Hallowell <melleehall@gmail.com>
2020-02-01 12:41:58 -0700

    added event listeners so that delete and check buttons work and new item can be added to list

--- src/shopping-list/ShoppingList.js
+++ src/shopping-list/ShoppingList.js
@@ -4,12 +4,14 @@ import ShoppingItem from './ShoppingItem';
 export default function ShoppingList(props) {
     return (
         <ul>
-            {props.items.map((item, i) => 
-                <ShoppingItem
-                key={i}
-                item={i}
+          {props.items.map((item, i) =>
+            <ShoppingItem
+              key={i}
+              item={item}
+              onDeleteItem={props.onDeleteItem}
+              onCheckItem={props.onCheckItem}
             />
-            )}
+          )}
         </ul>
     )
 }
