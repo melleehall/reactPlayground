cabde29c3d3734cc9da1dccfbc37677097b69324
Melanie Hallowell <melleehall@gmail.com>
2020-02-01 11:41:44 -0700

    Finished writing snapshot tests for the 4 components pre addition of event listeners

---
+++ src/shopping-list/AddItemForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class AddItemForm extends React.Component {
+    render() {
+        
+        return (
+            <form>
+                <input
+                    type='text'
+                    placeholder='carrots'
+                    aria-label='Shopping list item'
+                />
+                <button type='submit'>Add Item</button>
+            </form>
+        )
+    }
+}
